2cc173e2190f1913e59bc0ed1e2e0e455aaf4f55
Redempt <instantiable@gmail.com>
2022-09-07 13:26:07 -0400

    Begin implementing BNF parser

--- src/bnf/mod.rs
+++ src/bnf/mod.rs
@@ -1,12 +1,45 @@
 use crate::matchers::MatcherRef;
+use crate::error::{Result, FluxError};
 
-pub struct BNFParser {
+pub struct BNFParserState {
     pub source: Vec<char>,
     pub pos: usize,
 }
 
-impl BNFParser {
-    pub fn parse(&self) -> MatcherRef {
+impl BNFParserState {
+    pub fn parse(&self) -> Result<MatcherRef> {
         todo!()
+    }
+
+    pub fn peek(&self) -> Option<char> {
+        self.source.get(self.pos).map(|c| *c)
+    }
+
+    pub fn advance(&mut self) -> Option<char> {
+        let c = self.peek();
+        self.pos += 1;
+        return c;
+    }
+
+    pub fn assert_char(&mut self, match_char: char) -> Result<()> {
+        match self.advance() {
+            Some(c) if c == match_char => Ok(()),
+            _ => Err(FluxError::new("", self.pos))
+        }
+    }
+
+    pub fn assert_string(&mut self, match_string: &str) -> Result<()> {
+        for c in match_string.chars() {
+            self.assert_char(c)?
+        }
+        Ok(())
+    }
+
+    pub fn parse_word(&mut self) -> Result<String> {
+        let mut out = String::new();
+        while self.peek().map_or(false, |c| c.is_alphabetic()) {
+            out.push(self.advance().unwrap());
+        }
+        Ok(out)
     }
 }
