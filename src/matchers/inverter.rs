ed7f8eb15ff90018a9248a30bf1b9b9aecd44abc
FuzzyNovaGoblin <fuzzy.nova.goblin@outlook.com>
2022-09-07 16:26:39 -0400

    create inverter

--- src/matchers/inverter.rs
+++ src/matchers/inverter.rs
@@ -1,1 +1,36 @@
+use std::rc::Rc;
 
+use super::{Matcher, MatcherRef};
+
+pub struct Inverter {
+    name: Option<Rc<String>>,
+    child: MatcherRef,
+}
+
+impl Inverter {
+    pub fn new(child: MatcherRef) -> Self {
+        Self { name: None, child }
+    }
+}
+
+impl Matcher for Inverter {
+    fn apply(
+        &self,
+        source: Rc<Vec<char>>,
+        pos: usize,
+    ) -> crate::error::Result<crate::tokens::Token> {
+        todo!()
+    }
+
+    fn min_length(&self) -> usize {
+        todo!()
+    }
+
+    fn get_name(&self) -> Option<&str> {
+        todo!()
+    }
+
+    fn set_name(&mut self, new_name: String) {
+        todo!()
+    }
+}
