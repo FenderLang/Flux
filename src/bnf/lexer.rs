b1e21b873174f63ed5fa240d3780530a01c8a963
Redempt <instantiable@gmail.com>
2022-11-09 14:42:18 -0500

    Move lexer

--- src/bnf/lexer.rs
+++ src/bnf/lexer.rs
@@ -1,8 +1,11 @@
 use crate::matchers::MatcherRef;
 
 enum CullStrategy {
+    /// Delete the token and all of its children
     DELETE_ALL,
+    /// Delete the children of the token but not the token itself
     DELETE_CHILDREN,
+    /// Delete the token and replace it with its children in its parent
     LIFT_CHILDREN
 }
 
