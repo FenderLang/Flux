a85a27f54bc109c48783f8ff9e96b34567fa4e54
Redempt <instantiable@gmail.com>
2022-11-29 21:44:58 -0500

    Fix function invocation bnf

--- examples/demo.rs
+++ examples/demo.rs
@@ -1,10 +1,11 @@
-use flux::bnf;
+use flux::{bnf, lexer::CullStrategy};
 
 fn main() {
     let example = include_str!("fender.bnf");
-    let parsed = bnf::parse(example).unwrap();
-    let test = "(0+0)";
+    let mut lexer = bnf::parse(example).unwrap();
+    lexer.add_rule_for_names(vec!["alpha", "alphanum"].iter().map(|s| s.to_string()).collect(), CullStrategy::DeleteAll);
+    let test = "{}()";
     let thing = test.chars().collect::<Vec<_>>();
-    let token = parsed.tokenize(&thing);
+    let token = lexer.tokenize(&thing);
     println!("{:#?}", token);
 }
