2bd681af828e70e5043b7de1d014019c7552dc7e
Redempt <instantiable@gmail.com>
2022-11-30 00:48:23 -0500

    Allow operator-assign (+=)

--- examples/demo.rs
+++ examples/demo.rs
@@ -5,8 +5,8 @@ fn main() {
     let mut lexer = bnf::parse(example).unwrap();
     lexer.add_rule_for_names(vec!["alpha", "alphanum", "sep", "break", "lineBreak", "lineSep"].iter().map(|s| s.to_string()).collect(), CullStrategy::DeleteAll);
     let test = r#"
-    if {
-    }
+    $x = 4
+    x += 1
     "#;
     let thing = test.chars().collect::<Vec<_>>();
     let token = lexer.tokenize(&thing);
