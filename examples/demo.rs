b08625fda46f60113ea6436e0a6e846de2cdea5e
Redempt <instantiable@gmail.com>
2022-11-30 13:07:30 -0500

    Example

--- examples/demo.rs
+++ examples/demo.rs
@@ -5,7 +5,28 @@ fn main() {
     let mut lexer = bnf::parse(example).unwrap();
     lexer.add_rule_for_names(vec!["alpha", "alphanum", "sep", "break", "lineBreak", "lineSep"].iter().map(|s| s.to_string()).collect(), CullStrategy::DeleteAll);
     let test = r#"
-    1 1
+    $x = (a) {
+        $y = (b) {
+            return "hello world"
+        }
+        y(a)
+        "bad things"
+    }
+
+    $createor = {
+        $y = {
+            pass 7
+        }
+        y
+    }
+    $other_get_num = {
+        return 7
+    }
+
+    $b = x(0)
+    $get_num = creator()
+    getr_num() == other_get_num()
+    b() # Errors, since b is trying to return from x, which is no longer the function being invoked
     "#;
     let thing = test.chars().collect::<Vec<_>>();
     let token = lexer.tokenize(&thing);
