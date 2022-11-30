8eeaeaa4529753d5f466d3fbfa915b84d23a1269
Redempt <instantiable@gmail.com>
2022-11-30 01:26:59 -0500

    Enforce line breaks between statements in root context

--- examples/demo.rs
+++ examples/demo.rs
@@ -5,8 +5,7 @@ fn main() {
     let mut lexer = bnf::parse(example).unwrap();
     lexer.add_rule_for_names(vec!["alpha", "alphanum", "sep", "break", "lineBreak", "lineSep"].iter().map(|s| s.to_string()).collect(), CullStrategy::DeleteAll);
     let test = r#"
-    $x = 4
-    x += 1
+    1 1
     "#;
     let thing = test.chars().collect::<Vec<_>>();
     let token = lexer.tokenize(&thing);
