4ace22b0491bbd7cc0af6d4faef2854e2257407a
Redempt <instantiable@gmail.com>
2022-11-30 18:57:49 -0500

    Fix comments

--- examples/demo.rs
+++ examples/demo.rs
@@ -5,7 +5,17 @@ fn main() {
     let mut lexer = bnf::parse(example).unwrap();
     lexer.add_rule_for_names(vec!["alpha", "alphanum", "sep", "break", "lineBreak", "lineSep", "comment"].iter().map(|s| s.to_string()).collect(), CullStrategy::DeleteAll);
     let test = r#"
-$map = [:]
+    $func = (n) { # comment
+        (n < 1).then { # comment
+            "small" # comment
+        } # comment
+        (n > 10).then { # comment
+            # comment
+    # another comment
+            return "large"
+        }
+        return "medium"
+    }
     "#;
     let thing = test.chars().collect::<Vec<_>>();
     let token = lexer.tokenize(&thing);
