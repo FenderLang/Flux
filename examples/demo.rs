38c23c40d858f625ee5f0b5dd5c1a37caf53d2c5
Redempt <instantiable@gmail.com>
2022-11-30 16:29:43 -0500

    Use [:] for empty map

--- examples/demo.rs
+++ examples/demo.rs
@@ -5,7 +5,7 @@ fn main() {
     let mut lexer = bnf::parse(example).unwrap();
     lexer.add_rule_for_names(vec!["alpha", "alphanum", "sep", "break", "lineBreak", "lineSep", "comment"].iter().map(|s| s.to_string()).collect(), CullStrategy::DeleteAll);
     let test = r#"
-$map = <a: int, b, c>
+$map = [:]
     "#;
     let thing = test.chars().collect::<Vec<_>>();
     let token = lexer.tokenize(&thing);
