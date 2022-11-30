40953e00a1289add591173f76b971dfc5b31b06d
Redempt <instantiable@gmail.com>
2022-11-29 19:04:20 -0500

    Add parenthetical expressions

--- examples/demo.rs
+++ examples/demo.rs
@@ -3,7 +3,7 @@ use flux::bnf;
 fn main() {
     let example = include_str!("fender.bnf");
     let parsed = bnf::parse(example).unwrap();
-    let test = "0+0";
+    let test = "(0+0)";
     let thing = test.chars().collect::<Vec<_>>();
     let token = parsed.tokenize(&thing);
     println!("{:#?}", token);
