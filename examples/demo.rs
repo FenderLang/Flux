c81311282ecfaa19e9ad5b7c814cdd90adc2360b
Redempt <instantiable@gmail.com>
2022-11-30 00:17:08 -0500

    Fix whitespace consuming newlines in bnf

--- examples/demo.rs
+++ examples/demo.rs
@@ -4,7 +4,9 @@ fn main() {
     let example = include_str!("fender.bnf");
     let mut lexer = bnf::parse(example).unwrap();
     lexer.add_rule_for_names(vec!["alpha", "alphanum", "sep", "break", "lineBreak"].iter().map(|s| s.to_string()).collect(), CullStrategy::DeleteAll);
-    let test = r#"[1+1,2]"#;
+    let test = r#"0
+
+print("hello!")"#;
     let thing = test.chars().collect::<Vec<_>>();
     let token = lexer.tokenize(&thing);
     println!("{:#?}", token);
--- src/lexer.rs
+++ src/lexer.rs
@@ -1,4 +1,3 @@
-#![allow(dead_code)]
 use crate::error::{FluxError, Result};
 use crate::matchers::MatcherRef;
 use crate::tokens::Token;
