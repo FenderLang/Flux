965b8c485fdd25340b45ac868ac66a2f08c1de53
Redempt <instantiable@gmail.com>
2022-11-30 00:28:25 -0500

    Allow multiline expressions in more places

--- examples/demo.rs
+++ examples/demo.rs
@@ -3,9 +3,10 @@ use flux::{bnf, lexer::CullStrategy};
 fn main() {
     let example = include_str!("fender.bnf");
     let mut lexer = bnf::parse(example).unwrap();
-    lexer.add_rule_for_names(vec!["alpha", "alphanum", "sep", "break", "lineBreak"].iter().map(|s| s.to_string()).collect(), CullStrategy::DeleteAll);
+    lexer.add_rule_for_names(vec!["alpha", "alphanum", "sep", "break", "lineBreak", "lineSep"].iter().map(|s| s.to_string()).collect(), CullStrategy::DeleteAll);
     let test = r#"
-    (a) {}
+    if {
+    }
     "#;
     let thing = test.chars().collect::<Vec<_>>();
     let token = lexer.tokenize(&thing);
