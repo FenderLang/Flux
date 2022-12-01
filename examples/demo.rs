80e40defffa5d3301c4359c672989717c13cb9e3
FuzzyNovaGoblin <fuzzy.nova.goblin@outlook.com>
2022-11-30 19:17:54 -0500

    create tests

--- examples/demo.rs
+++ examples/demo.rs
@@ -1,7 +1,7 @@
 use flux::{bnf, lexer::CullStrategy};
 
 fn main() {
-    let example = include_str!("fender.bnf");
+    let example = include_str!("../src/tests/bnf/fender.bnf");
     let mut lexer = bnf::parse(example).unwrap();
     lexer.add_rule_for_names(vec!["alpha", "alphanum", "sep", "break", "lineBreak", "lineSep", "comment"].iter().map(|s| s.to_string()).collect(), CullStrategy::DeleteAll);
     let test = r#"
