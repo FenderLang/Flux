fadd71c7e40654a15c01c2a8163f709277b7f1b7
FuzzyNovaGoblin <fuzzy.nova.goblin@outlook.com>
2022-12-08 13:12:19 -0500

    use clone on range instead of making a new range from start to end of src range

--- src/tokens.rs
+++ src/tokens.rs
@@ -1,5 +1,5 @@
 use crate::matchers::MatcherName;
-use std::{ops::Range, fmt::Debug};
+use std::{fmt::Debug, ops::Range};
 
 #[derive(Clone)]
 pub struct Token<'a> {
@@ -12,14 +12,17 @@ pub struct Token<'a> {
 
 impl<'a> Token<'a> {
     fn get_match(&self) -> String {
-        self.source[self.range.start..self.range.end].iter().collect()
+        self.source[self.range.clone()].iter().collect()
     }
 }
 
 impl<'a> Debug for Token<'a> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let mut debug = f.debug_struct("Token");
-        debug.field("name", &self.matcher_name.borrow().clone().unwrap_or("".to_string()));
+        debug.field(
+            "name",
+            &self.matcher_name.borrow().clone().unwrap_or("".to_string()),
+        );
         debug.field("match", &self.get_match());
         debug.field("range", &self.range);
         if !self.children.is_empty() {
