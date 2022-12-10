55215dc4b594f4fdbabdb6527ae691cfbad87c48
Redempt <instantiable@gmail.com>
2022-12-09 20:58:38 -0500

    Add import statements

--- examples/demo.rs
+++ examples/demo.rs
@@ -5,9 +5,7 @@ fn main() {
     let mut lexer = bnf::parse(example).unwrap();
     lexer.add_rule_for_names(vec!["alpha", "alphanum", "sep", "break", "lineBreak", "lineSep", "comment"].iter().map(|s| s.to_string()).collect(), CullStrategy::DeleteAll);
     let test = r#"
-    $point = <x:int, y:int>
-    $tmp = point(7,12)
-    tmp:x.print()
+    import *std:file
     "#;
     let thing = test.chars().collect::<Vec<_>>();
     let token = lexer.tokenize(&thing);
